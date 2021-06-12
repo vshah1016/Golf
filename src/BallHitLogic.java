c288a91a93494f10da4f44d937412f7a2b3317f1
phegde494 <48624928+phegde494@users.noreply.github.com>
2021-06-12 18:22:01 -0400

    Update BallHitLogic.java

--- src/BallHitLogic.java
+++ src/BallHitLogic.java
@@ -1,17 +1,6 @@
 public class BallHitLogic {
    //TODO PRAJWAL FIX THIS YOU IDIOT
-    public static void uponContact(int power, int vert, int hor) //ratio = y/x
-    {
-        for (int i = 0; i < power; i++) {
-            y += vert;
-            x += hor;
-            list.add(new Point(x, y);
-            bounceCheck();
-            render();
-            power--;
-
-        }
-
+    
         public static int[] bounceCheck ( int vert, int hor) //This is the important method here
         {
             for (int i = 0; i < obstacles.size(); i++) {
@@ -35,14 +24,4 @@ public class BallHitLogic {
             }
 
         }
-//OBSTACLE HAS PROPERTY BOTTOM LEFT, BOTTOM RIGHT, TOP LEFT, and TOP RIGHT
-        public static boolean includes( int x, int y) //DW About this
-        {
-            int x1 = Math.min(bottomLeft.getY(), topLeft.getY());
-            int x2 = Math.max(bottomRight.getY(), topRight.getY());
-            int y1 = Math.min(bottomLeft.getX(), bottomRight.getX());
-            int y2 = Math.max(bottomLeft.getX(), bottomRight.getX());
-           return x >= x1 && x <= x2 && y >= bottomLeft.getY() && y <= topRight.getY();
-        }
-    }
 }
