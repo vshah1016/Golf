f39077ea40a56f5e1584c6591af378cd2cc573b9
phegde494 <48624928+phegde494@users.noreply.github.com>
2021-06-12 16:59:05 -0400

    Add files via upload

---
+++ src/BallHitLogic.java
@@ -0,0 +1,49 @@
+public void uponContact(int power, int vert, int hor) //ratio = y/x
+{
+   for(int i = 0; i < power; i++)
+   {
+      y+= vert;
+      x+= hor;
+      list.add(new Point(x,y);
+      bounceCheck();
+      render();
+      power--;
+
+}
+
+public int[] bounceCheck(int vert, int hor) //This is the important method here
+{
+   for(int i = 0; i < obstacles.size(); i++)
+   {
+      int [][] points = hitting();
+      if(points[0][0] != -1) 
+      {
+         int changeY = (points[1][1] - points[0][1]);
+         int changeX = (points[1][0]-points[0][0]); 
+//Slope of final trajectory = wallSlope/(initialDirection/wallSlope);
+
+         int diffY = changeX*vert;
+         int diffX = changeY*hor;
+         
+         int newY = diffX*y;
+         int newX = diffY*x;
+         int [] arr = new int[2];
+         arr[0] = newX;
+         arr[1] = newY;
+         return arr;
+         
+      }
+   }
+
+}
+//OBSTACLE HAS PROPERTY BOTTOM LEFT, BOTTOM RIGHT, TOP LEFT, and TOP RIGHT
+public boolean includes(int x, int y) //DW About this
+{
+   int x1 = Math.min(bottomLeft.getY(), topLeft.getY());
+   int x2 = Math.max(bottomRight.getY(), topRight.getY());
+   int y1 = Math.min(bottomLeft.getX(), bottomRight.getX());
+   int y2 = Math.max(bottomLeft.getX(), bottomRight.getX());
+   if(x >= x1 && x <= x2 && y >= bottomLeft.getY() && y <= topRight.getY())
+      return true;
+   return false;
+}
