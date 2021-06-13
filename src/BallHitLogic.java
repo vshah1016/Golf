6a8a563b25e12e689736a6b15784d5c21758cab6
phegde494 <48624928+phegde494@users.noreply.github.com>
2021-06-13 10:53:00 -0400

    Update BallHitLogic.java

--- src/BallHitLogic.java
+++ src/BallHitLogic.java
@@ -1,27 +1,27 @@
-//public class BallHitLogic {
-//   //TODO PRAJWAL FIX THIS YOU IDIOT
-//
-//        public static int[] bounceCheck ( int vert, int hor) //This is the important method here
-//        {
-//            for (int i = 0; i < obstacles.size(); i++) {
-//                int[][] points = hitting();
-//                if (points[0][0] != -1) {
-//                    int changeY = (points[1][1] - points[0][1]);
-//                    int changeX = (points[1][0] - points[0][0]);
-////Slope of final trajectory = wallSlope/(initialDirection/wallSlope);
-//
-//                    int diffY = changeX * vert;
-//                    int diffX = changeY * hor;
-//
-//                    int newY = diffX * y;
-//                    int newX = diffY * x;
-//                    int[] arr = new int[2];
-//                    arr[0] = newX;
-//                    arr[1] = newY;
-//                    return arr;
-//
-//                }
-//            }
-//
-//        }
-//}
+public class BallHitLogic {
+  //TODO PRAJWAL FIX THIS YOU IDIOT
+
+       public static int[] bounceCheck ( int vert, int hor) //This is the important method here
+       {
+           for (int i = 0; i < obstacles.size(); i++) {
+               int[][] points = hitting();
+               if (points[0][0] != -1) {
+                   int changeY = (points[1][1] - points[0][1]);
+                   int changeX = (points[1][0] - points[0][0]);
+//Slope of final trajectory = wallSlope/(initialDirection/wallSlope);
+
+                   int diffY = changeX * vert;
+                   int diffX = changeY * hor;
+
+                   int newY = diffX * y;
+                   int newX = diffY * x;
+                   int[] arr = new int[2];
+                   arr[0] = newX;
+                   arr[1] = newY;
+                   return arr;
+
+               }
+           }
+
+       }
+}
