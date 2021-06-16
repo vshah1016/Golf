1b25f7d20d64f5273feab6616e9f3a63a2e79f55
Veer <veer.shah1016@gmail.com>
2021-06-16 01:41:09 -0400

    suryaa use this

---
+++ src/BouncePath.java
@@ -0,0 +1,11 @@
+public class BouncePath {
+    public final double m;
+    public final double b;
+    public final Point intersection;
+
+    public BouncePath(double m, double b, Point intersection) {
+        this.m = m;
+        this.b = b;
+        this.intersection = intersection;
+    }
+}
