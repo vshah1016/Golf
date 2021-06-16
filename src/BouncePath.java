f5d66952738f9c1ae629d2989e65fcfa73fe667e
Veer <veer.shah1016@gmail.com>
2021-06-16 02:21:47 -0400

    new parameter

--- src/BouncePath.java
+++ src/BouncePath.java
@@ -3,11 +3,13 @@ public class BouncePath {
     public final double b;
     public final Point intersection;
     public final int direction; //0: L, 1:R, 2: U, 3:D
+    public final boolean bounce;
 
-    public BouncePath(double m, double b, Point intersection, int direction) {
+    public BouncePath(double m, double b, Point intersection, int direction, boolean bounce) {
         this.m = m;
         this.b = b;
         this.intersection = intersection;
         this.direction = direction;
+        this.bounce = bounce;
     }
 }
