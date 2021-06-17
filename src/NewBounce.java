0720f91516d2e931828a009d46429d0a9b977db1
Veer <veer.shah1016@gmail.com>
2021-06-16 21:31:37 -0400

    start loop at 1 bozo... remember the hole

--- src/NewBounce.java
+++ src/NewBounce.java
@@ -6,16 +6,25 @@ public class NewBounce {
     public static BouncePath bounce(double m, double b, int direction, int ballCurrX, int ballCurrY){
         Shape[] obstacles = Main.obstacles;
         ArrayList<Integer> arrX = new ArrayList<>();
-        for(Shape shape: obstacles){
-            double [][] coors= shape.coors;
-            double topX = ((coors[1][1]-b)/m);
-            double botX = ((coors[0][1]-b)/m);
-            double leftY = m*coors[0][0]+b;
-            double rightY = m*coors[1][0]+b;
-            if(topX>=coors[0][0]&&topX<=coors[1][0]){arrX.add((int)topX);}
-            if(botX>=coors[0][0]&&botX<=coors[1][0]){arrX.add((int)botX);}
-            if(leftY>=coors[0][1]&&leftY<=coors[1][1]){arrX.add((int)coors[0][0]);}
-            if(rightY>=coors[0][1]&&rightY<=coors[1][1]){arrX.add((int)coors[1][0]);}
+        for (int i = 1; i < obstacles.length; i++) {
+            Shape shape = obstacles[i];
+            double[][] coors = shape.coors;
+            double topX = ((coors[1][1] - b) / m);
+            double botX = ((coors[0][1] - b) / m);
+            double leftY = m * coors[0][0] + b;
+            double rightY = m * coors[1][0] + b;
+            if (topX >= coors[0][0] && topX <= coors[1][0]) {
+                arrX.add((int) topX);
+            }
+            if (botX >= coors[0][0] && botX <= coors[1][0]) {
+                arrX.add((int) botX);
+            }
+            if (leftY >= coors[0][1] && leftY <= coors[1][1]) {
+                arrX.add((int) coors[0][0]);
+            }
+            if (rightY >= coors[0][1] && rightY <= coors[1][1]) {
+                arrX.add((int) coors[1][0]);
+            }
         }
         Collections.sort(arrX);
         if(arrX.size()>0){
