ee2e83ca7611941ca3dc72d289c8688b402cc5df
Suryaa <32228906+Suryaa1234567890@users.noreply.github.com>
2021-06-16 21:02:32 -0400

    please work bruh

--- src/NewBounce.java
+++ src/NewBounce.java
@@ -5,32 +5,27 @@ import java.util.Collections;
 public class NewBounce {
     int deltaY;
     int deltaX;
+    double slope;
     int b;
     int direction;
     int ballCurrX;
     int ballCurrY;
     public BouncePath bounce(Shape[] obstacles){
 
-        int j=ballCurrY;
         ArrayList<Integer> arrX = new ArrayList<>();
-        for(int i=ballCurrX; i<=1920; i+=deltaX){
-            for(Shape shape: obstacles){
-                double [][] coors = shape.coors;
-                if(coors[1][1]==j&&i>=coors[0][0]&&i<=coors[1][0]){
-                    arrX.add(i);
-                }
-                else if(coors[0][1]==j&&i>=coors[0][0]&&i<=coors[1][0]){
-                    arrX.add(i);
-                }
-                else if(coors[0][0]==i&&j>=coors[0][1]&&j<=coors[0][1]){
-                    arrX.add(i);
-                }
-                else if(coors[0][1]==i&&j>=coors[0][1]&&j<=coors[0][1]){
-                    arrX.add(i);
-                }
-            }
-            j+=deltaY;
+
+        for(Shape shape: obstacles){
+            double [][] coors= shape.coors;
+            double topX = ((coors[1][1]-b)/slope);
+            double botX = ((coors[0][1]-b)/slope);
+            double leftY = slope*coors[0][0]+b;
+            double rightY = slope*coors[1][0]+b;
+            if(topX>=coors[0][0]&&topX<=coors[1][0]){arrX.add((int)topX);}
+            if(botX>=coors[0][0]&&botX<=coors[1][0]){arrX.add((int)botX);}
+            if(leftY>=coors[0][1]&&leftY<=coors[1][1]){arrX.add((int)coors[0][0]);}
+            if(rightY>=coors[0][1]&&rightY<=coors[1][1]){arrX.add((int)coors[1][0]);}
         }
+
         Collections.sort(arrX);
         if(arrX.size()>0){
             if (direction == 1) {
